8fff10101e069d96dba6a1c7c79242d4ffc7f05e
Kirill Danilkov <k.danilkov.work@gmail.com>
2023-05-15 11:57:04 +0300

    initial commit

---
+++ account/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .forms import LoginForm
+
+
+def user_login(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            user = authenticate(request,
+                                username=cleaned_data['username'],
+                                password=cleaned_data['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request: HttpRequest) -> HttpResponse:
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
